e49a5cc8013c62676adef20fbb25e169b36e9b95
Jim Taylor <jim.taylor@highlights.com>
2015-03-17 06:04:20 -0400

    Applying pylint recommendations to updates.py

--- drupdates/updates.py
+++ drupdates/updates.py
@@ -1,42 +1,43 @@
-from drupdates.utils import *
-from drupdates.constructors.repos import *
-from drupdates.constructors.pmtools import *
-from drupdates.constructors.reports import *
-from drupdates.constructors.datastores import *
+""" Primary Drupdates Module. """
+from drupdates.settings import Settings
+from drupdates.constructors.repos import repos
+from drupdates.constructors.pmtools import pmtools
+from drupdates.constructors.reports import reports
+from drupdates.constructors.datastores import datastores
 from drupdates.sitebuild import Sitebuild
-from drupdates.siteupdate import *
+from drupdates.siteupdate import Siteupdate
 
 def main():
-  settings = Settings()
-  report = {}
-  sites = repos().get()
-  pmTool = pmtools()
-  utilities = utils()
-  blacklist = settings.get('blacklist')
-  singleSite = settings.get('singleSite')
-  datastore = datastores()
-  datastore.createAlises()
-  if singleSite:
-    sites = {singleSite : sites[singleSite]}
-  for siteName, ssh in sites.iteritems():
-    report[siteName] = {}
-    if siteName in blacklist:
-      continue
+    """ Drupdates main function. """
+    settings = Settings()
+    report = {}
+    sites = repos().get()
+    pm_tool = pmtools()
+    blacklist = settings.get('blacklist')
+    single_site = settings.get('singleSite')
+    datastore = datastores()
+    datastore.createAlises()
+    if single_site:
+        sites = {single_site : sites[single_site]}
+    for site_name, ssh in sites.iteritems():
+        report[site_name] = {}
+        if site_name in blacklist:
+            continue
 
-    if settings.get('buildRepos'):
-      builder = Sitebuild(siteName, ssh)
-      build = builder.build()
-      if not build:
-        continue
+        if settings.get('buildRepos'):
+            builder = Sitebuild(site_name, ssh)
+            build = builder.build()
+            if not build:
+                continue
 
-    if settings.get('runUpdates'):
-      updater = Siteupdate(siteName, ssh)
-      update = updater.update()
-      report[siteName] = update
-      if settings.get('submitDeployTicket') and updater.commit_hash:
-        deploys = pmTool.deployTicket(siteName, updater.commit_hash)
-        report[siteName]['pmtool'] = deploys
+        if settings.get('runUpdates'):
+            updater = Siteupdate(site_name, ssh)
+            update = updater.update()
+            report[site_name] = update
+            if settings.get('submitDeployTicket') and updater.commit_hash:
+                deploys = pm_tool.deployTicket(site_name, updater.commit_hash)
+                report[site_name]['pmtool'] = deploys
 
-  datastore.cleanFiles()
-  reporting = reports()
-  reporting.send(report)
+    datastore.cleanFiles()
+    reporting = reports()
+    reporting.send(report)
