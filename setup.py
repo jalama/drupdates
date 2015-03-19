950ed0492d89ac0f94606d91e2d805ac4d6107ce
Jim Taylor <jim.taylor@highlights.com>
2015-03-19 08:35:04 -0400

    Bump version to 1.2.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     url='https://github.com/jalama/drupdates',
     download_url='https://github.com/jalama/drupdates',
     author_email='jim@rootyhollow.com',
-    version='1.1.6',
+    version='1.2.0',
     package_dir={'drupdates' : 'drupdates'},
     include_package_data=True,
     install_requires=['nose', 'gitpython', 'requests', 'pyyaml'],
