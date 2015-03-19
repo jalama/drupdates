3a8bf8eeb5cde6a18c35a2ad5bd1828ebda99984
Jim Taylor <jim.taylor@highlights.com>
2015-03-18 21:25:12 -0400

    Resolves #92 - Applying pylint recommendations to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
+""" Drupdates setup script. """
 try:
-	from setuptools import setup, find_packages
+    from setuptools import setup
 except ImportError:
-	from distutils.core import setup, find_packages
+    from distutils.core import setup
 import sys
 
 # Ensure Python verison is >= 2.6
@@ -10,37 +11,37 @@ if not sys.version_info[0] < 2 and sys.version_info[1] < 6:
     print "Python version needs to exceed 2.6"
     sys.exit(1) # return non-zero value for failure
 
-setup (
-	name = 'Drupdates',
-	description = 'Drupal updates scripts',
-	author = 'Jim Taylor',
-	url = 'https://github.com/jalama/drupdates',
-	download_url = 'https://github.com/jalama/drupdates',
-	author_email = 'jim@rootyhollow.com',
-	version = '1.1.6',
-	package_dir = {'drupdates' : 'drupdates'},
-	include_package_data = True,
-	install_requires = ['nose', 'gitpython', 'requests', 'pyyaml'],
-	entry_points = {
-		'console_scripts': ['drupdates = drupdates.updates:main'],
-	},
-	packages = ['drupdates'],
-	classifiers=[
-	  'Development Status :: 4 - Beta',
-	  'Environment :: Console',
-	  'Intended Audience :: Developers',
-	  'Intended Audience :: System Administrators',
-	  'License :: OSI Approved :: MIT License',
-	  'Natural Language :: English',
-	  'Operating System :: MacOS :: MacOS X',
-	  'Operating System :: POSIX',
-	  'Programming Language :: Python',
-	  'Programming Language :: Python :: 2',
-	  'Programming Language :: Python :: 2.6',
-	  'Programming Language :: Python :: 2.7',
-	  'Topic :: System :: Systems Administration',
-	  'Topic :: Software Development :: Build Tools',
-	  'Topic :: Software Development :: Bug Tracking',
-	  ],
+setup(
+    name='Drupdates',
+    description='Drupal updates scripts',
+    author='Jim Taylor',
+    url='https://github.com/jalama/drupdates',
+    download_url='https://github.com/jalama/drupdates',
+    author_email='jim@rootyhollow.com',
+    version='1.1.6',
+    package_dir={'drupdates' : 'drupdates'},
+    include_package_data=True,
+    install_requires=['nose', 'gitpython', 'requests', 'pyyaml'],
+    entry_points={
+        'console_scripts': ['drupdates = drupdates.updates:main'],
+    },
+    packages=['drupdates'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Bug Tracking',
+    ],
 )
 
