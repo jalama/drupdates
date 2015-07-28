89d5156a93187a72c6904a14c36603988abf0ca8
Jim Taylor <jim.taylor@highlights.com>
2015-07-28 07:38:17 -0400

    Update version to 1.3.1 update email address

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ setup(
     author='Jim Taylor',
     url='https://github.com/jalama/drupdates',
     download_url='https://github.com/jalama/drupdates',
-    author_email='jim@rootyhollow.com',
-    version='1.3.0',
+    author_email='jalama@gmail.com',
+    version='1.3.1',
     package_dir={'drupdates' : 'drupdates'},
     include_package_data=True,
     install_requires=['nose', 'gitpython', 'requests', 'pyyaml'],
@@ -44,4 +44,3 @@ setup(
         'Topic :: Software Development :: Bug Tracking',
     ],
 )
-
