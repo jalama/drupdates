c891976fb241c51ad83e45c901fc53894f3b2565
Jim Taylor <jim@rootyhollow.com>
2015-08-26 22:25:56 -0400

    Add Python 3.4 to travis tests and setup.py

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',        
         'Topic :: System :: Systems Administration',
         'Topic :: Software Development :: Build Tools',
         'Topic :: Software Development :: Bug Tracking',
