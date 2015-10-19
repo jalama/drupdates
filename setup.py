18ece12dce0d76afc9215b7fa2eee3bcbb966302
Jim Taylor <jalama@gmail.com>
2015-10-18 22:17:07 -0400

    Officialy add support for Python 3.5

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: System :: Systems Administration',
         'Topic :: Software Development :: Build Tools',
         'Topic :: Software Development :: Bug Tracking',
